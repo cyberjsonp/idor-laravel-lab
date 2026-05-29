b1755209ccde6f2cb67d7f5f1d432f7fdf52c9d6
cyberjsonp <cyberjson@outlook.com>
2026-05-29 18:51:35 +0330

    start IDOR With Laravel Lab

---
+++ app/Http/Controllers/Http/Controllers/Challenges/ChallengeIndexController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Http\Controllers\Challenges;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ChallengeIndexController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        //
+    }
+}
